6907b1f6af35e6f07b07003d19d037cb95bdb0c6
noman.ali.abbasy@gmail.com <Engrabbasi1>
2018-04-24 11:00:20 +0500

    Added MapReactiveUserDetailsService for custom login

--- src/main/java/edu/learn/config/SecurityConfig.java
+++ src/main/java/edu/learn/config/SecurityConfig.java
@@ -4,11 +4,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+    /**
+     * Once you implemented this you will get There is no PasswordEncoder mapped
+     * to resolve it we need to specify password encoder
+     * @return
+     */
+    @Bean
+    MapReactiveUserDetailsService userDetailsService() {
+        UserDetails noman = User.withUsername("n@abbasi.com")
+                .password("x")
+                .roles("USER")
+                .build();
+
+        return new MapReactiveUserDetailsService(noman);
+    }
+
+    /**
+     * Following example is taken from @see WebFluxSecurityConfiguration in EnableWebFluxSecurity
+     */
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
     http.authorizeExchange().pathMatchers("/login","/signup","/webjars/**").permitAll()
